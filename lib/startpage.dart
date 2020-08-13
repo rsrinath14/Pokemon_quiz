c5afee001ebe0ff6e1c84e3e1b4a04d32ad5d450
R_Srinath <srinath1412001@gmail.com>
2020-08-13 08:25:26 +0530

    changes made

--- lib/startpage.dart
+++ lib/startpage.dart
@@ -1,6 +1,6 @@
+import 'dart:async';
 import 'dart:convert';
 import 'package:flutter/material.dart';
-import './Options.dart';
 
 class getjson extends StatelessWidget {
   @override
@@ -27,7 +27,6 @@ class getjson extends StatelessWidget {
 
 class StartPage extends StatefulWidget {
   List mydata;
-
   StartPage({Key key, @required this.mydata}) : super(key: key);
 
   @override
@@ -37,6 +36,93 @@ class StartPage extends StatefulWidget {
 class _StartPageState extends State<StartPage> {
   List mydata;
   _StartPageState(this.mydata);
+
+  Color colortoshow = Colors.blueGrey[900];
+  Color colorright = Colors.green;
+  Color colorwrong = Colors.red;
+  int marks = 0;
+  int i = 1;
+  int timer = 10;
+  String showTimer = "10";
+
+  Widget Options(String k) {
+    return Padding(
+      padding: EdgeInsets.symmetric(
+        vertical: 10.0,
+        horizontal: 20.0,
+      ),
+      child: MaterialButton(
+        onPressed: () => checkanswer(k),
+        child: Text(
+          mydata[1][i.toString()][k],
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 16.0,
+          ),
+        ),
+        color: btncolor[k],
+        splashColor: Colors.blueGrey[700],
+        highlightColor: Colors.blueGrey[700],
+        minWidth: 200.0,
+        height: 45.0,
+        shape:
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+      ),
+    );
+  }
+
+  Map<String, Color> btncolor = {
+    "a": Colors.blueGrey[900],
+    "b": Colors.blueGrey[900],
+    "c": Colors.blueGrey[900],
+    "d": Colors.blueGrey[900],
+  };
+
+  void initState() {
+    starttimer();
+    super.initState();
+  }
+
+  void nextquestion() {
+    setState(() {
+      if (i < 10) {
+        i++;
+      } else {}
+      btncolor["a"] = Colors.blueGrey[900];
+      btncolor["b"] = Colors.blueGrey[900];
+      btncolor["c"] = Colors.blueGrey[900];
+      btncolor["d"] = Colors.blueGrey[900];
+    });
+  }
+
+  void starttimer() async {
+    const onesec = Duration(seconds: 1);
+    Timer.periodic(onesec, (Timer t) {
+      setState(() {
+        if (timer < 1) {
+          t.cancel();
+          nextquestion();
+        } else {
+          timer = timer - 1;
+        }
+        showTimer = timer.toString();
+      });
+    });
+  }
+
+  void checkanswer(String k) {
+    if (mydata[1]["1"][k] == mydata[2]["1"]) {
+      marks = marks + 1;
+      colortoshow = colorright;
+    } else {
+      colortoshow = colorwrong;
+    }
+    setState(() {
+      btncolor[k] = colortoshow;
+    });
+
+    Timer(Duration(seconds: 1), nextquestion);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -51,7 +137,7 @@ class _StartPageState extends State<StartPage> {
                   child: Container(
                     alignment: Alignment.center,
                     child: Image.asset(
-                      mydata[0][3.toString()],
+                      mydata[0][i.toString()],
                     ),
                   ),
                 )),
@@ -60,10 +146,10 @@ class _StartPageState extends State<StartPage> {
                 child: Container(
                   child: Column(
                     children: <Widget>[
-                      Options('Pikachu'),
-                      Options('Tauros'),
-                      Options('Pigiot'),
-                      Options('Onyx'),
+                      Options('a'),
+                      Options('b'),
+                      Options('c'),
+                      Options('d'),
                     ],
                   ),
                 )),
@@ -72,7 +158,7 @@ class _StartPageState extends State<StartPage> {
                 child: Container(
                     alignment: Alignment.topCenter,
                     child: Text(
-                      "10",
+                      showTimer,
                       style: TextStyle(
                         fontSize: 30,
                         fontWeight: FontWeight.bold,
